e2dbc7ce60e275dcb5886748b8b5114b95198bac
Shaahir Meeran Mohiadeen M I <shaahirmeeranmohaideen2510883@ssn.edu.in>
2026-08-03 23:36:59 +0530

    Create 162-FindPeakElement

---
+++ 162-FindPeakElement/162-FindPeakElement.cpp
@@ -0,0 +1,32 @@
+// Last updated: 8/3/2026, 11:37:00 PM
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+        int n =  nums.size();
+
+        if(n == 1) return 0;
+        if(nums[0] > nums[1]) return 0;
+        else if(nums[n-1] > nums[n-2]) return n-1;
+
+        int low = 1;
+        int high = n-1;
+
+        while(low<=high){
+            int mid = low + (high - low)/2;
+
+            if(nums[mid-1]<nums[mid] && nums[mid]>nums[mid+1]){
+                return mid;
+            }
+            // increasing slope
+            else if(nums[mid-1]<nums[mid]){
+                low = mid+1;
+            }
+            //decreasing slope
+            else{
+                high = mid-1;
+            }
+        }
+
+        return -1;
+    }
+};
