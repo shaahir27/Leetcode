17617ef792ba5ca61a25e0386ede0ed8cfe961a9
Shaahir Meeran Mohiadeen M I <shaahirmeeranmohaideen2510883@ssn.edu.in>
2026-08-03 23:22:05 +0530

    Create 493-ReversePairs

---
+++ 493-ReversePairs/493-ReversePairs.cpp
@@ -0,0 +1,79 @@
+// Last updated: 8/3/2026, 11:22:07 PM
+class Solution {
+private:
+    void merge(vector<int>& arr, int low, int mid, int high){
+
+        vector<int> temp;
+        int left = low;
+        int right = mid+1;
+
+        while(left<=mid && right<=high){
+
+            if(arr[left] <= arr[right]){
+                temp.push_back(arr[left]);
+                left++;
+            }
+
+            else{
+                temp.push_back(arr[right]);
+                right++;
+            }
+        }
+
+        while(left<=mid){
+            temp.push_back(arr[left]);
+            left++;
+        }
+
+        while(right<=high){
+            temp.push_back(arr[right]);
+            right++;
+        }
+
+        for(int i=low; i<=high; i++){
+            arr[i] = temp[i-low];
+        }
+
+        return;
+    }
+
+    int countPairs(vector<int>& arr, int low, int mid, int high){
+
+        int count = 0;
+        int right = mid+1;
+
+        for(int i=low; i<=mid; i++){
+            while(right<=high && arr[i] > 2LL*arr[right]) right++;
+            count += right - (mid + 1);
+        }
+        
+        return count;
+
+    }
+
+    int mergeSort(vector<int>& arr, int low, int high){
+
+        int count = 0;
+        if(low>=high) return count;
+
+        int mid = (low+high)/2;
+
+        count += mergeSort(arr, low, mid);
+        count += mergeSort(arr, mid+1, high);
+        count += countPairs(arr, low, mid, high);
+
+        merge(arr, low, mid, high);
+
+        return count;
+    }
+
+public:
+    int reversePairs(vector<int>& nums) {
+
+        int count;
+        int n = nums.size();
+        count = mergeSort(nums, 0, n-1);
+
+        return count;
+    }
+};
