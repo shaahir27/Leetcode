df52a6bf2ef1a116fa5fc1130e9774a734a1ba25
Shaahir Meeran Mohiadeen M I <shaahirmeeranmohaideen2510883@ssn.edu.in>
2026-08-03 23:21:44 +0530

    Create 1086-DivisorGame

---
+++ 1086-DivisorGame/1086-DivisorGame.cpp
@@ -0,0 +1,10 @@
+// Last updated: 8/3/2026, 11:21:45 PM
+class Solution {
+public:
+    bool divisorGame(int n) {
+        if(n%2 == 0){
+            return true;
+        }
+        return false;
+    }
+};
