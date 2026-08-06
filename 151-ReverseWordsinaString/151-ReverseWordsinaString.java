09d211bf8b73922483d4bfcdd7f7714eeec83617
Shaahir Meeran Mohiadeen M I <shaahirmeeranmohaideen2510883@ssn.edu.in>
2026-08-06 23:13:14 +0530

    Create 151-ReverseWordsinaString

---
+++ 151-ReverseWordsinaString/151-ReverseWordsinaString.java
@@ -0,0 +1,29 @@
+// Last updated: 8/6/2026, 11:13:15 PM
+1class Solution {
+2    public String reverseWords(String s) {
+3        s = s.trim();
+4
+5        String current = "";
+6        String reverse = "";
+7
+8        int i = 0;
+9
+10        while (i < s.length()) {
+11
+12            if (s.charAt(i) != ' ') {
+13                current += s.charAt(i);
+14            }
+15            else if (!current.isEmpty()) {
+16                reverse = current + " " + reverse;
+17                current = "";
+18            }
+19
+20            i++;
+21        }
+22
+23        if(!current.isEmpty()){
+24            reverse = current + " " + reverse;
+25        }
+26        return reverse.trim();
+27    }
+28}
