49c24be7792e9d051081891fc1370f5880e11b41
Shaahir Meeran Mohiadeen M I <shaahirmeeranmohaideen2510883@ssn.edu.in>
2026-08-18 22:37:36 +0530

    File updated at 8/18/2026, 10:37:35 PM

--- 14-LongestCommonPrefix/14-LongestCommonPrefix.cpp
+++ 14-LongestCommonPrefix/14-LongestCommonPrefix.cpp
@@ -1,28 +1,23 @@
-// Last updated: 8/3/2026, 11:38:25 PM
-class Solution {
-public:
-    string longestCommonPrefix(vector<string>& strs) {
-        
-        string common = "";
-        sort(strs.begin(), strs.end());
-
-        int n = strs.size();
-
-        string first_element = strs[0];
-        string last_element = strs[n-1];
-
-
-
-        for(int i = 0; i<min(first_element.size(), last_element.size()); i++){
-            if(first_element[i] == last_element[i]){
-                common += first_element[i];
-            }
-            else{
-                break;
-            }
-        }
-
-        return common;
-
-    }
-};
+// Last updated: 8/18/2026, 10:37:35 PM
+1class Solution {
+2public:
+3    string longestCommonPrefix(vector<string>& strs) {
+4        int n = strs.size();
+5        string result = "";
+6
+7        sort(strs.begin(), strs.end());
+8
+9        int i = 0;
+10
+11        while(strs[0][i] && strs[n-1][i]){
+12            if(strs[0][i] == strs[n-1][i]){
+13                result += strs[0][i];
+14                i++;
+15                continue;
+16            }
+17            break;
+18        }
+19
+20        return result;
+21    }
+22};
