b08dd87e99682e45c97705b5e262e5f93dae22aa
Shaahir Meeran Mohiadeen M I <shaahirmeeranmohaideen2510883@ssn.edu.in>
2026-08-20 23:08:46 +0530

    File updated at 8/20/2026, 11:08:46 PM

--- 242-ValidAnagram/242-ValidAnagram.cpp
+++ 242-ValidAnagram/242-ValidAnagram.cpp
@@ -1,11 +1,28 @@
-// Last updated: 8/20/2026, 11:04:30 PM
+// Last updated: 8/20/2026, 11:08:46 PM
 1class Solution {
 2public:
 3    bool isAnagram(string s, string t) {
-4        sort(s.begin(), s.end());
-5        sort(t.begin(), t.end());
-6
-7        if(s == t) return true;
-8        else return false;
-9    }
-10};
+4        if(s.length() != t.length()) return false;
+5
+6        int frequency[26] = {0};
+7
+8        for(int i=0; i<s.length(); i++){
+9            frequency[s[i] - 'a']++;
+10            frequency[t[i] - 'a']--;
+11        }
+12
+13        for(int i=0; i<26; i++){
+14            if(frequency[i] != 0) return false;
+15        }
+16
+17        return true;
+18
+19        /*
+20        sort(s.begin(), s.end());
+21        sort(t.begin(), t.end());
+22
+23        if(s == t) return true;
+24        else return false;
+25        */
+26    }
+27};
