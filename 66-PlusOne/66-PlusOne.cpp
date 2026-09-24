f3d4673dec63b08a0fe4e0d57f8139b508f41105
Shaahir Meeran Mohiadeen M I <shaahirmeeranmohaideen2510883@ssn.edu.in>
2026-09-24 10:50:58 +0530

    File updated at 9/24/2026, 10:50:59 AM

--- 66-PlusOne/66-PlusOne.cpp
+++ 66-PlusOne/66-PlusOne.cpp
@@ -1,25 +1,22 @@
-// Last updated: 9/24/2026, 10:48:49 AM
+// Last updated: 9/24/2026, 10:50:59 AM
 1class Solution {
 2public:
 3    vector<int> plusOne(vector<int>& digits) {
 4        int carry = 1;
 5
 6        for(int i=digits.size()-1; i>=0; i--){
-7            int sum = digits[i] + carry;
-8            carry = sum/10;
-9
-10            if(carry > 0){
-11                digits[i] = sum%10;
-12            }
-13            else{
-14                digits[i] = sum;
-15            }
-16        }
-17
-18        if(carry > 0){
-19            digits.insert(digits.begin(), 1);
-20        }
-21
-22        return digits;
-23    }
-24};
+7            if(digits[i] < 9){
+8                digits[i]++;
+9                return digits;
+10            }
+11
+12            digits[i] = 0;
+13        }
+14
+15        if(carry > 0){
+16            digits.insert(digits.begin(), 1);
+17        }
+18
+19        return digits;
+20    }
+21};
